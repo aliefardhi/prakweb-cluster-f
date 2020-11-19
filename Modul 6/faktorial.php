303dd01a64d89476eb4636ca2bf874fa644059c6
aliefardhi <alief.118140003@student.itera.ac.id>
2020-11-19 12:36:22 +0700

    add file faktorial

---
+++ Modul 6/faktorial.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $n = 5;
+
+    for($i=0; $i<$n; $i++){
+        $faktorial *= $n;
+    }
+    echo "Faktorial $n = " . $faktorial;
+?>
+</body>
+</html>
+
