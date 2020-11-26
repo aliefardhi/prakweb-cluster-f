8d977b53e8916f8c90a6f56a13da64a387413fc9
aliefardhi <alief.118140003@student.itera.ac.id>
2020-11-26 15:10:43 +0700

    upload latihan modul 8

---
+++ Modul 8/latihan/index.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    //redirect jika mempunyai session
+    if(isset($_SESSION['user'])){
+        header('location:home.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Login Authentication</title>
+</head>
+<body>
+    <h1>PHP Login</h1>
+    <h3><span></span>Login</h3>
+    <form action="login.php" method="POST">
+        <div>
+            <input type="text" name="username" placeholder="Username.." autofocus required>
+        </div>
+        <div>
+            <input type="password" name="password" placeholder="Password.." required>
+        </div>
+        <button type="submit" name="login"><span></span> Login</button>
+    </form>
+
+    <?php
+        if(isset($_SESSION['message'])){
+            ?>
+            <div>
+                <?=$_SESSION['message'];?>
+            </div>
+            <?php unset($_SESSION['message']);
+        }
+    ?>
+</body>
+</html>
